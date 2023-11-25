ce3e38df1588eff2be6814989b9a89b2e373f7ba
zulanggara <zul.anggara8@gmail.com>
2023-11-25 22:45:19 +0700

    finish

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +17,16 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $request->validated();
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password_confirmation' => Hash::make($request->password_confirmation) ?? NULL
+        ];
+        if ($request->password) {
+            $data['password'] = Hash::make($request->password);
+        }
+        Auth::user()->update($data);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
